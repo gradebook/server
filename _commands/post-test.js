ba2f9bd0180f2626da4f5e8040b9c5ebad0af7ae
Vikas Potluri <vikaspotluri123.github@gmail.com>
2019-09-18 20:12:39 -0500

    wrap post-test command in try/catch block

--- _commands/post-test.js
+++ _commands/post-test.js
@@ -1,28 +1,33 @@
-const crypto = require('crypto');
-const got = require('got');
+try {
+	const crypto = require('crypto');
+	const got = require('got');
 
-const REQUIRED_KEYS = ['GITHUB_SHA', 'GITHUB_REF', 'GITHUB_WORKFLOW', 'WEBHOOK_URL', 'WEBHOOK_SECRET', 'TEST_NAME'];
+	const REQUIRED_KEYS = ['GITHUB_SHA', 'GITHUB_REF', 'GITHUB_WORKFLOW', 'WEBHOOK_URL', 'WEBHOOK_SECRET', 'TEST_NAME'];
 
-for (const key of REQUIRED_KEYS) {
-	if (!process.env.hasOwnProperty(key)) {
-		console.warn(`Missing key: ${key}. Not running post-test hook`);
-		return;
+	for (const key of REQUIRED_KEYS) {
+		if (!(key in process.env)) {
+			console.warn(`Missing key: ${key}. Not running post-test hook`);
+			process.exit(0);
+		}
 	}
+
+	const payload = JSON.stringify({
+		commit: process.env.GITHUB_SHA,
+		branch: process.env.GITHUB_REF.split('/').pop(),
+		name: process.env.TEST_NAME
+	});
+
+	const hmac = crypto.createHmac('sha256', Buffer.from(process.env.WEBHOOK_SECRET)).update(payload).digest('hex');
+
+	console.log('Sending payload', payload, 'to webhook');
+
+	got.post(process.env.WEBHOOK_URL, {
+		headers: {
+			'X-Actions-Secret': `sha256=${hmac}`
+		},
+		body: payload
+	});
+} catch (error) {
+	console.error(error);
+	process.exit(1);
 }
-
-const payload = JSON.stringify({
-	commit: process.env.GITHUB_SHA,
-	branch: process.env.GITHUB_REF.split('/').pop(),
-	name: process.env.TEST_NAME
-});
-
-const hmac = crypto.createHmac('sha256', Buffer.from(process.env.WEBHOOK_SECRET)).update(payload).digest('hex');
-
-console.log('Sending payload', payload, 'to webhook');
-
-got.post(process.env.WEBHOOK_URL, {
-	headers: {
-		'X-Actions-Secret': `sha256=${hmac}`,
-	},
-	body: payload
-});
