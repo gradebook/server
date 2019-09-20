996c1a580be4a919aee6a08230256a9bea3679e5
Vikas Potluri <vikaspotluri123.github@gmail.com>
2019-09-20 09:37:26 -0500

    Add ref and event checks to post-test webhook

--- _commands/post-test.js
+++ _commands/post-test.js
@@ -2,13 +2,33 @@ try {
 	const crypto = require('crypto');
 	const got = require('got');
 
-	const REQUIRED_KEYS = ['GITHUB_REPOSITORY', 'GITHUB_SHA', 'GITHUB_REF', 'GITHUB_WORKFLOW', 'WEBHOOK_URL', 'WEBHOOK_SECRET', 'TEST_NAME'];
+	const REQUIRED_KEYS = [
+		'GITHUB_REPOSITORY',
+		'GITHUB_EVENT_NAME',
+		'GITHUB_SHA',
+		'GITHUB_REF',
+		'GITHUB_WORKFLOW',
+		'WEBHOOK_URL',
+		'WEBHOOK_SECRET',
+		'TEST_NAME'
+	];
 
 	for (const key of REQUIRED_KEYS) {
 		if (!(key in process.env)) {
 			console.warn(`Missing key: ${key}. Not running post-test hook`);
 			process.exit(0);
 		}
+	}
+
+	if (process.env.GITHUB_EVENT_NAME !== 'push') {
+		console.warn(`Event ${process.env.GITHUB_EVENT_NAME} was not push, not running post-test hook`);
+		process.exit(0);
+	}
+
+	// @todo: Update
+	if (process.env.GITHUB_REF !== 'refs/heads/hooks') {
+		console.warn(`Ref ${process.env.GITHUB_REF} was not for release branch, not running post-test hook`);
+		process.exit(0);
 	}
 
 	const payload = JSON.stringify({
@@ -28,9 +48,7 @@ try {
 		},
 		body: payload
 	});
-	console.log(require('fs').readFileSync(process.env.GITHUB_EVENT_PATH, 'utf8'));
 } catch (error) {
-	console.log(require('fs').readFileSync(process.env.GITHUB_EVENT_PATH, 'utf8'));
 	console.error(error);
 	process.exit(1);
 }
