a047274b3397fd4360ec76580a84dcea4b975341
Vikas Potluri <vikaspotluri123.github@gmail.com>
2019-10-22 20:56:40 -0500

    Bump node version in command precheck

--- _commands/_precheck.js
+++ _commands/_precheck.js
@@ -9,7 +9,7 @@ const properDir = path.resolve(__dirname, '../');
 const readFile = promisify(fs.readFile);
 const writeFile = promisify(fs.writeFile);
 
-const MAJOR_MINOR_MATCH = 'v10.16.';
+const MAJOR_MINOR_MATCH = 'v12.13.';
 
 let execa;
 try {
