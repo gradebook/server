506f518dcb9b3e5ee8129ace04d6cfe8f0fd44e1
Vikas Potluri <vikaspotluri123.github@gmail.com>
2019-12-06 21:55:31 -0600

    Add course.edit schema

---
+++ test/unit/schemas/edit-course.js
@@ -0,0 +1,20 @@
+const schemaValidator = require('../../utils/schema-validator');
+const cutSchema = require('../../../lib/services/validation/schemas/course-cut.json');
+const schema = require('../../../lib/services/validation/schemas/edit-course.json');
+
+describe('Unit > Schemas > EditCourse', function () {
+	const {expectInvalid, expectValid} = schemaValidator(schema, [cutSchema]);
+
+	it('protected props', function () {
+		expectInvalid({}, ['keyword', 'minProperties'], 'have fewer than 1');
+		expectInvalid({semester: '2019S'}, ['keyword', 'additionalProperties'], 'NOT have additional properties');
+	});
+
+	it('allows changing different permutations', function () {
+		expectValid({name: 'New Course'});
+		expectValid({cutA: 120});
+		expectValid({cutB: 40});
+		expectValid({cutC: 84});
+		expectValid({cutD: 94});
+	});
+});
