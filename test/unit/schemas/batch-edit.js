00accca76abbd0df35364374ea1cf9a52ca12d00
Vikas Potluri <vikaspotluri123.github@gmail.com>
2019-12-07 17:59:46 -0600

    Add grades.batch schema and migrate validator to use it

---
+++ test/unit/schemas/batch-edit.js
@@ -0,0 +1,108 @@
+const ObjectId = require('bson-objectid');
+const schemaValidator = require('../../utils/schema-validator');
+const objSchema = require('../../../lib/services/validation/schemas/object-id.json');
+const schema = require('../../../lib/services/validation/schemas/batch-edit.json');
+
+describe('Unit > Schemas > BatchEditGrades', function () {
+	const {expectInvalid, expectValid} = schemaValidator(schema, [objSchema]);
+
+	it('Bad all around', function () {
+		expectInvalid({}, ['keyword', 'minProperties'], 'NOT have fewer than 1 properties');
+		expectInvalid({
+			create: [],
+			update: []
+		}, ['keyword', 'minItems'], 'NOT have fewer than 1 items');
+
+		expectInvalid({
+			delete: [],
+		}, ['keyword', 'minItems'], 'NOT have fewer than 1 items');
+
+		expectInvalid({
+			update: [],
+		}, ['keyword', 'minItems'], 'NOT have fewer than 1 items');
+
+		expectInvalid({
+			create: [],
+		}, ['keyword', 'minItems'], 'NOT have fewer than 1 items');
+	});
+
+	it('Only create', function () {
+		expectValid({
+			create: [{
+				name: 'Homework 1',
+				grade: 95
+			}, {
+				name: 'Homework 2',
+				grade: null
+			}, {
+				name: 'Homework 3'
+			}]
+		});
+
+		expectInvalid({
+			create: {
+				name: 'test'
+			}
+		}, ['keyword', 'type'], 'should be array');
+
+		expectInvalid({
+			create: [{
+				id: {
+					test: ObjectId.generate()
+				}
+			}]
+		}, ['dataPath', '.create[0]'], 'NOT have additional properties');
+
+		expectInvalid({
+			create: [{
+				category_id: ObjectId.generate() // eslint-disable-line camelcase
+			}]
+		}, ['dataPath', '.create[0]'], 'NOT have additional properties');
+	});
+
+	it('Only update', function () {
+		expectValid({
+			update: [{
+				id: ObjectId.generate(),
+				name: 'Homework 1',
+				grade: 100
+			}, {
+				id: ObjectId.generate(),
+				grade: 85
+			}, {
+				id: ObjectId.generate(),
+				grade: null
+			}]
+		});
+	});
+
+	it('Only delete', function () {
+		expectValid({
+			delete: [ObjectId.generate(), ObjectId.generate(), ObjectId.generate()]
+		});
+
+		const duplicateId = ObjectId.generate();
+		expectInvalid({
+			delete: [duplicateId, duplicateId]
+		}, ['dataPath', '.delete'], 'NOT have duplicate items');
+
+		expectInvalid({
+			delete: ObjectId.generate()
+		}, ['dataPath', '.delete'], 'should be array');
+	});
+
+	it('No create AND delete', function () {
+		expectInvalid({
+			delete: [ObjectId.generate()],
+			create: [{
+				name: 'Homework 1',
+				grade: 95
+			}, {
+				name: 'Homework 2',
+				grade: null
+			}, {
+				name: 'Homework 3'
+			}]
+		}, ['schemaPath', '#/dependencies/delete/not'], 'NOT be valid');
+	});
+});
