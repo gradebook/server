e612e98b66b53f64f4063a77db80e11deeee48df
Vikas Potluri <vikaspotluri123.github@gmail.com>
2019-12-07 18:00:04 -0600

    Add logging param in schema-validator util for test development

--- test/utils/schema-validator.js
+++ test/utils/schema-validator.js
@@ -11,11 +11,20 @@ module.exports = function createSchemaValidator(schema, additionalSchemas = []) 
 			expect(ajv.validate(schema, payload)).to.be.ok;
 		},
 
-		expectInvalid(payload, kv = [], messageMatch = '') {
+		expectInvalid(payload, kv = [], messageMatch = '', logError = false) {
+			if ('CI' in process.env) {
+				expect(logError, 'SchemaValidation: logError is a *debug tool* and should not be used in tests').to.not.be.ok;
+			}
+
 			expect(ajv.validate(schema, payload)).to.not.be.ok;
 			expect(kv.length, 'Error is properly validated').to.equal(2);
 
 			const {errors} = ajv;
+			if (logError) {
+				// eslint-disable-next-line no-console
+				console.log(errors);
+			}
+
 			const [key, value] = kv;
 			expect(errors[0][key]).to.equal(value);
 			expect(errors[0].message).to.contain(messageMatch);
