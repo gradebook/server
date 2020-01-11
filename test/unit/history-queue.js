b7f7973d40422f440102b8af039f9de914f9be81
Vikas Potluri <vikaspotluri123.github@gmail.com>
2020-01-11 12:25:13 -0600

    Update analytics to support multiple databases

--- test/unit/history-queue.js
+++ test/unit/history-queue.js
@@ -20,32 +20,32 @@ describe('Unit > HistoryQueue', function () {
 
 	describe('Process Event', function () {
 		it('normal operations', async function () {
-			queue.add([1, 1, 1]);
-			queue.add([2, 3, 5]);
-			queue.add([1, 1, 3]);
-			queue.add([5, 20, 50]);
-			queue.add([3, 5, 10]);
+			queue.add([null, 1, 1, 1]);
+			queue.add([null, 2, 3, 5]);
+			queue.add([null, 1, 1, 3]);
+			queue.add([null, 5, 20, 50]);
+			queue.add([null, 3, 5, 10]);
 
 			await queue._promise;
 
 			expect(queue.empty).to.be.false;
-			expect(queue.internalSummations).to.be.an('array').and.to.deep.equal([12, 30, 69]);
+			expect(queue._summations.get('__NO_DATABASE__')).to.be.an('array').and.to.deep.equal([12, 30, 69]);
 		});
 
 		it('properly handles buffering while committing', async function () {
 			// Seed the queue and wait for seed events to be processed
-			queue.add([1]);
-			queue.add([1]);
-			queue.add([1]);
+			queue.add([null, 1]);
+			queue.add([null, 1]);
+			queue.add([null, 1]);
 			await queue._promise;
 
-			expect(queue.internalSummations).to.be.an('array').and.to.deep.equal([3]);
+			expect(queue._summations.get('__NO_DATABASE__')).to.be.an('array').and.to.deep.equal([3]);
 
 			// Add 3 elements while the queue is committing
 			const promise = queue.commit();
-			queue.add([2]);
-			queue.add([2]);
-			queue.add([2]);
+			queue.add([null, 2]);
+			queue.add([null, 2]);
+			queue.add([null, 2]);
 			await promise;
 
 			// Wait for the queue to empty so we can make sure no data was lost
@@ -54,10 +54,10 @@ describe('Unit > HistoryQueue', function () {
 			await queue.process();
 
 			expect(queue._list).to.be.empty;
-			expect(queue.internalSummations).to.deep.equal([6]);
+			expect(queue._summations.get('__NO_DATABASE__')).to.deep.equal([6]);
 
 			await queue.commit();
-			expect(queue.internalSummations).to.be.empty;
+			expect(queue._summations.get('__NO_DATABASE__')).to.be.empty;
 		});
 
 		it('does not commit when analytics are disabled', async function () {
@@ -66,23 +66,23 @@ describe('Unit > HistoryQueue', function () {
 			try {
 				queue.pause = testUtils.expectError;
 
-				queue.add([0]);
+				queue.add([null, 0]);
 				await queue._promise;
-				expect(queue.internalSummations).to.be.an('array').with.length(1);
+				expect(queue._summations.get('__NO_DATABASE__')).to.be.an('array').with.length(1);
 				await queue.commit();
 				expect(stub.calledOnce).to.be.true;
-				expect(queue.internalSummations).to.be.an('array').and.is.empty;
+				expect(queue._summations.get('__NO_DATABASE__')).to.be.an('array').and.is.empty;
 
 				// @todo: stub.reset() is throwing an error for some reason
 				config.get.restore();
 				stub = sinon.stub(config, 'get').withArgs('analytics').returns(false);
 
-				queue.add([0]);
+				queue.add([null, 0]);
 				await queue._promise;
-				expect(queue.internalSummations).to.be.an('array').with.length(1);
+				expect(queue._summations.get('__NO_DATABASE__')).to.be.an('array').with.length(1);
 				await queue.commit();
 				expect(stub.calledOnce).to.be.true;
-				expect(queue.internalSummations).to.be.an('array').and.is.empty;
+				expect(queue._summations.get('__NO_DATABASE__')).to.be.an('array').and.is.empty;
 			} finally {
 				config.get.restore();
 			}
