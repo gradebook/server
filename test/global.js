76f306c971cf70bcc2b1f6ba82cafd24a42805ef
Vikas Potluri <vikaspotluri123.github@gmail.com>
2020-02-08 11:42:13 -0600

    Move test configuration to nconf
    no issue
    
    - the original implementation stopped working in 4d8f422 but wasn't noticed until now
    - the old implementation was always hacky, but now that we use nconf, we can use a non-hacky normal solution

--- test/global.js
+++ test/global.js
@@ -1,9 +1,7 @@
-const {join} = require('path');
 const {expect} = require('chai');
 const sinon = require('sinon');
 
 process.env.NODE_ENV = 'testing';
-process.env.AGB_DATABASE_PATH = join(__dirname, './fixtures/test-database.db');
 
 global.expect = expect;
 global.sinon = sinon;
