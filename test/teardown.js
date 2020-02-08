80ddfd4761ae9fa37fb88c693490028689b084ed
Vikas Potluri <vikaspotluri123.github@gmail.com>
2020-02-08 12:34:45 -0600

    test.teardown: stop all cron jobs

--- test/teardown.js
+++ test/teardown.js
@@ -1,4 +1,7 @@
 after(() => {
 	const knex = require('../lib/database/knex');
 	knex.instance.destroy();
+
+	const cron = require('../lib/services/cron/init');
+	cron._tasks.forEach(task => task.stop());
 });
