b454ac20463cde2eafff3d8ce37eb6e32dad69f5
Josh Costa <joshcos97@gmail.com>
2020-04-16 23:04:27 -0500

    importCourse validation unit tests

--- test/unit/validation.js
+++ test/unit/validation.js
@@ -1,5 +1,6 @@
 const root = '../../lib/services/validation';
 
+const settings = require('../../lib/services/settings');
 const {validations} = require(root);
 const expectError = require('../utils/expect-error');
 
@@ -157,6 +158,102 @@ describe('Unit > Validation', function () {
 				req.body.cut1 = 95;
 
 				validations.editCourse(req, null, done);
+			});
+		});
+	});
+
+	describe('Import Course', function () {
+		const createRequest = () => ({
+			body: {
+				course: {
+					name: 'ECEN 482',
+					semester: '2019S',
+					credits: 3,
+					cut1: 90,
+					cut2: 80,
+					cut3: 70,
+					cut4: 60,
+					cut1Name: 'A+',
+					cut2Name: 'B',
+					cut3Name: 'D-',
+					cut4Name: 'C+'
+				},
+				categories: [
+					{name: 'Single', weight: 40, position: 100, numGrades: 1, dropped: null},
+					{name: 'Expanded', weight: 60, position: 200, numGrades: 3, dropped: 1}
+				]
+			}, params: {id: '5dc10582a8109cd864bd8a13'}, user: {id: '5dc1069b2ff198252ca3b596'}
+		});
+
+		describe('Only allows valid number of categories', function () {
+			it('Too many categories fails', function () {
+				const req = createRequest();
+				req.body.categories = [];
+
+				for (let i = 0; i < 15; i++) {
+					req.body.categories.push({name: 'Single', weight: 40, position: 100, numGrades: 1, dropped: null});
+				}
+
+				const stub = sinon.stub(settings, 'get').returns(10);
+
+				try {
+					validations.importCourse(req, null, expectError);
+					expectError();
+				} catch (error) {
+					expect(error.message).to.include('too many categories');
+					stub.restore();
+				}
+			});
+
+			it('Reasonable number of categories passes', function (done) {
+				const req = createRequest();
+				req.body.categories = [];
+
+				for (let i = 0; i < 5; i++) {
+					req.body.categories.push({name: 'Single', weight: 40, position: 100, numGrades: 1, dropped: null});
+				}
+
+				const stub = sinon.stub(settings, 'get').returns(10);
+
+				validations.importCourse(req, null, done);
+				stub.restore();
+			});
+
+			it('No categories passes', function (done) {
+				const req = createRequest();
+				req.body.categories = [];
+
+				const stub = sinon.stub(settings, 'get').returns(10);
+
+				validations.importCourse(req, null, done);
+				stub.restore();
+			});
+		});
+
+		describe('Only allows valid number of grades', function () {
+			it('Too many grades fails', function () {
+				const req = createRequest();
+				req.body.categories[0] = {name: 'Single', weight: 40, position: 100, numGrades: 50, dropped: null};
+
+				const stub = sinon.stub(settings, 'get').returns(40);
+
+				try {
+					validations.importCourse(req, null, expectError);
+					expectError();
+				} catch (error) {
+					expect(error.message).to.include('too many grades');
+					stub.restore();
+				}
+			});
+
+			it('Reasonable number of grades passes', function (done) {
+				const req = createRequest();
+				req.body.categories[0] = {name: 'Single', weight: 40, position: 100, numGrades: 10, dropped: null};
+
+				const stub = sinon.stub(settings, 'get').returns(40);
+
+				validations.importCourse(req, null, done);
+				stub.restore();
 			});
 		});
 	});
