2146e7ea7d1b52d040e10f12a1ad286b2377e72f
Vikas Potluri <vikaspotluri123.github@gmail.com>
2020-07-05 13:23:27 -0500

    Force unit tests to be stuck to 2019S

--- test/global.js
+++ test/global.js
@@ -6,3 +6,9 @@ process.env.NODE_ENV = 'testing';
 global.expect = expect;
 global.sinon = sinon;
 global.testUtils = require('./utils');
+
+// Force the active semester to be Spring 2019
+const semesterService = require('../lib/services/current-semester');
+
+semesterService.activeSemester = '2019S';
+semesterService.validSemesters = new Set(['2019S']);
