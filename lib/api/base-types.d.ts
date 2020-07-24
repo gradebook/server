dd4db91c9ec5fee12a275c22129f71ec455c4d4d
Vikas Potluri <vikaspotluri123.github@gmail.com>
2020-07-24 13:14:17 -0500

    Add types to grade api

--- lib/api/base-types.d.ts
+++ lib/api/base-types.d.ts
@@ -1,5 +1,5 @@
 type BrowseResponse<T> = (filterOptions: T, db: string) => Promise<any>
-type BrowseFilterFunction<T> = (currentOptions: any, filters: T) => object | void
+type BrowseFilterFunction<T> = (currentOptions: any, filters: T) => import('knex').QueryCallback | void
 
 export type BaseBrowse<AllowedFilters extends {}> = (
 	dataType: string,
