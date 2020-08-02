c5251e2b216624c570a920e3cf4cfe838f1b9d4d
Vikas Potluri <vikaspotluri123.github@gmail.com>
2020-08-02 02:02:37 -0500

    Rework validation layer (#133)

---
+++ lib/database/schema.d.ts
@@ -0,0 +1,45 @@
+export interface TableSchema {
+	[column: string]: ColumnSchema;
+}
+
+type PrimaryOrUnique = {
+	primary: true;
+	unique: true;
+} | {
+	unique ?: true;
+	primary?: false;
+}
+
+type BaseSchema = PrimaryOrUnique & {
+	index?: boolean;
+	nullable?: boolean;
+	references?: string;
+	fallback?: string | number | boolean;
+}
+
+type StringColumn = BaseSchema & {
+	type: 'string';
+	maxLength: number;
+}
+
+type TextColumn = BaseSchema & {
+	type: 'text',
+	subType?: 'tinytext' | 'mediumtext' | 'longtext' | 'json',
+	validations: {
+		maxLength: number;
+	};
+}
+
+type NumericColumn = BaseSchema & {
+	type: 'integer' | 'tinyint' | 'smallint' | 'mediumint' | 'bigint' | 'float';
+	unsigned?: boolean;
+	validations: {
+		between: [number, number]
+	}
+}
+
+type BasicColumn = BaseSchema & {
+	type: 'date' | 'datetime' | 'timestamp' | 'text';
+};
+
+export type ColumnSchema = StringColumn | TextColumn | NumericColumn | BasicColumn;
