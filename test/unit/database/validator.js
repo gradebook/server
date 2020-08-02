7bafc6e8badf2f3655eb0ead6d420034da5a2e37
Vikas Potluri <vikaspotluri123.github@gmail.com>
2020-08-02 01:34:58 -0500

    refactor validator checks to run against _validateSingleColumn
    - no reason to validate an entire model when we're trying to test a single column

--- test/unit/database/validator.js
+++ test/unit/database/validator.js
@@ -6,50 +6,135 @@ const validator = require('../../../lib/database/validator');
 const schema = require('../../../lib/database/schema');
 const {ValidationError} = require('../../../lib/errors');
 
-class FakeModel {
-	constructor() {
-		this.data = {
-			id: 'a'.repeat(24),
-			optional: 'o'.repeat(24),
-			text: 't'.repeat(24),
-			created: '2020-02-01',
-			dropped: 34,
-			tinyInt: 0,
-			integer: 0,
-			float: 12.15,
-			grade: 100,
-			boolean: true
-		};
-
-		this._changed = {};
-	}
-
-	get(key) {
-		return this.data[key];
-	}
-
-	set(key, value) {
-		this._changed[key] = value;
-	}
-
-	changed(key) {
-		return key in this._changed;
-	}
-}
-
-/**
- * @param {import('../../../lib/database/validator').MinimumViableModel} model
- * @param {'insert' | 'update'} method
- */
-const runTest = (model, method = 'insert') => {
-	validator.validateRow('__test__', model, {method});
-};
+const NO_CHANGE = [null, undefined];
 
 describe('Unit > Validator', function () {
-	let testSchema;
+	describe('numeric types have out-of-bounds protection', function () {
+		it('tinyInt', function () {
+			const schema = {type: 'tinyint'};
+			const error = 'tinyInt is not within the allowed range';
 
-	before(function () {
-		testSchema = {
+			expect(validator._validateSingleColumn(schema, -129, 'tinyInt')).to.deep.equal([error, null]);
+			expect(validator._validateSingleColumn(schema, -128, 'tinyInt')).to.deep.equal(NO_CHANGE);
+			expect(validator._validateSingleColumn(schema, 127, 'tinyInt')).to.deep.equal(NO_CHANGE);
+			expect(validator._validateSingleColumn(schema, 128, 'tinyInt')).to.deep.equal([error, null]);
+
+			schema.unsigned = true;
+			expect(validator._validateSingleColumn(schema, -1, 'tinyInt')).to.deep.equal([error, null]);
+			expect(validator._validateSingleColumn(schema, 0, 'tinyInt')).to.deep.equal(NO_CHANGE);
+			expect(validator._validateSingleColumn(schema, 255, 'tinyInt')).to.deep.equal(NO_CHANGE);
+			expect(validator._validateSingleColumn(schema, 256, 'tinyInt')).to.deep.equal([error, null]);
+
+			schema.validations = {between: [0, 1]};
+			expect(validator._validateSingleColumn(schema, -1, 'tinyInt')).to.deep.equal([error, null]);
+			expect(validator._validateSingleColumn(schema, 0, 'tinyInt')).to.deep.equal(NO_CHANGE);
+			expect(validator._validateSingleColumn(schema, 1, 'tinyInt')).to.deep.equal(NO_CHANGE);
+			expect(validator._validateSingleColumn(schema, 2, 'tinyInt')).to.deep.equal([error, null]);
+		});
+
+		it('integer', function () {
+			const schema = {type: 'integer'};
+			const error = 'int is not within the allowed range';
+			expect(validator._validateSingleColumn(schema, -2147483649, 'int')).to.deep.equal([error, null]);
+			expect(validator._validateSingleColumn(schema, -2147483648, 'int')).to.deep.equal(NO_CHANGE);
+			expect(validator._validateSingleColumn(schema, 2147483647, 'int')).to.deep.equal(NO_CHANGE);
+			expect(validator._validateSingleColumn(schema, 2147483648, 'int')).to.deep.equal([error, null]);
+
+			schema.unsigned = true;
+			expect(validator._validateSingleColumn(schema, -1, 'int')).to.deep.equal([error, null]);
+			expect(validator._validateSingleColumn(schema, 0, 'int')).to.deep.equal(NO_CHANGE);
+			expect(validator._validateSingleColumn(schema, 4294967295, 'int')).to.deep.equal(NO_CHANGE);
+			expect(validator._validateSingleColumn(schema, 4294967296, 'int')).to.deep.equal([error, null]);
+		});
+
+		it('float', function () {
+			const schema = {type: 'float'};
+			const error = 'float is not within the allowed range';
+			expect(validator._validateSingleColumn(schema, -1000000.01, 'float')).to.deep.equal([error, null]);
+			expect(validator._validateSingleColumn(schema, -999999, 'float')).to.deep.equal(NO_CHANGE);
+			expect(validator._validateSingleColumn(schema, 999999, 'float')).to.deep.equal(NO_CHANGE);
+			expect(validator._validateSingleColumn(schema, 1000000.01, 'float')).to.deep.equal([error, null]);
+
+			schema.unsigned = true;
+			expect(validator._validateSingleColumn(schema, -1, 'float')).to.deep.equal([error, null]);
+			expect(validator._validateSingleColumn(schema, 0, 'float')).to.deep.equal(NO_CHANGE);
+			expect(validator._validateSingleColumn(schema, 999999, 'float')).to.deep.equal(NO_CHANGE);
+			expect(validator._validateSingleColumn(schema, 1000000.01, 'float')).to.deep.equal([error, null]);
+		});
+	});
+
+	it('numeric check handles empty values', function () {
+		const schema = {type: 'tinyint'};
+
+		expect(
+			validator._validateSingleColumn(schema, undefined, 'tinyInt')
+		).to.deep.equal(['tinyInt cannot be empty', null]);
+
+		schema.nullable = true;
+		expect(validator._validateSingleColumn(schema, undefined, 'tinyInt')).to.deep.equal([null, null]);
+
+		schema.fallback = 10;
+		expect(validator._validateSingleColumn(schema, undefined, 'tinyInt')).to.deep.equal([null, 10]);
+	});
+
+	it('boolean checks properly reject and coerce values', function () {
+		const schema = {type: 'boolean'};
+
+		expect(
+			validator._validateSingleColumn(schema, undefined, 'boolean')
+		).to.deep.equal(['boolean cannot be empty', null]);
+		expect(validator._validateSingleColumn(schema, false, 'boolean')).to.deep.equal(NO_CHANGE);
+		expect(validator._validateSingleColumn(schema, 'false', 'boolean')).to.deep.equal([null, false]);
+		expect(validator._validateSingleColumn(schema, '0', 'boolean')).to.deep.equal([null, false]);
+		expect(validator._validateSingleColumn(schema, true, 'boolean')).to.deep.equal(NO_CHANGE);
+		expect(validator._validateSingleColumn(schema, 'true', 'boolean')).to.deep.equal([null, true]);
+		expect(validator._validateSingleColumn(schema, '1', 'boolean')).to.deep.equal([null, true]);
+		expect(
+			validator._validateSingleColumn(schema, 'potato', 'boolean')
+		).to.deep.equal(['boolean must be Boolean', null]);
+		expect(validator._validateSingleColumn(schema, 2, 'boolean')).to.deep.equal(['boolean must be Boolean', null]);
+	});
+
+	it('null-ish values are accepted for nullable columns', function () {
+		const schema = {type: 'string', maxLength: 24, nullable: true};
+		expect(validator._validateSingleColumn(schema, null, 'optional')).to.deep.equal([null, null]);
+	});
+
+	it('null-ish values are rejected for non-nullable columns', function () {
+		const schema = {type: 'string', maxLength: 24, nullable: false};
+		expect(validator._validateSingleColumn(schema, null, 'id')).to.deep.equal(['id cannot be null', null]);
+		expect(validator._validateSingleColumn(schema, '', 'id')).to.deep.equal(['id cannot be empty', null]);
+	});
+
+	it('maxLength - strings and text blobs', function () {
+		const schema = {type: 'string', maxLength: 24, nullable: false};
+		expect(
+			validator._validateSingleColumn(schema, 'b'.repeat(25), 'id')
+		).to.deep.equal(['id exceeds the maxLength of 24', null]);
+
+		schema.nullable = true;
+
+		expect(
+			validator._validateSingleColumn(schema, 'b'.repeat(25), 'optional')
+		).to.deep.equal(['optional exceeds the maxLength of 24', null]);
+		expect(validator._validateSingleColumn(schema, 'o'.repeat(24), 'optional')).to.deep.equal(NO_CHANGE);
+	});
+
+	it('additional validation - between ', function () {
+		const schema = {type: 'tinyint', nullable: true, fallback: null, validations: {between: [1, 40]}};
+
+		expect(validator._validateSingleColumn(schema, 1, 'dropped')).to.deep.equal(NO_CHANGE);
+		expect(validator._validateSingleColumn(schema, 40, 'dropped')).to.deep.equal(NO_CHANGE);
+		expect(
+			validator._validateSingleColumn(schema, 0, 'dropped')
+		).to.deep.equal(['dropped is not within the allowed range', null]);
+		expect(
+			validator._validateSingleColumn(schema, 41, 'dropped')
+		).to.deep.equal(['dropped is not within the allowed range', null]);
+	});
+
+	it('general model validation works', function () {
+		const testSchema = {
 			// NOTE: only using fields required by the validator
 			id: {type: 'string', maxLength: 24, nullable: false},
 			optional: {type: 'string', maxLength: 24, nullable: true},
@@ -65,291 +150,45 @@ describe('Unit > Validator', function () {
 
 		// @ts-expect-error
 		schema.__test__ = testSchema;
-	});
+		const original = {
+			id: '25',
+			optional: 51,
+			text: 'c'.repeat(25),
+			dropped: null,
+			tinyInt: 12,
+			integer: -10,
+			float: 1.05,
+			grade: -1,
+			boolean: false
+		};
 
-	after(function () {
-		// @ts-expect-error
-		delete schema.__test__;
-	});
+		const diff = {};
+		const set = (key, value) => {
+			diff[key] = value;
+		};
 
-	describe('numeric types have out-of-bounds protection', function () {
-		function wrapRunTest(property, value, expectFailure = false) {
-			const model = new FakeModel();
-
-			model.data[property] = value;
-
-			if (expectFailure) {
-				try {
-					runTest(model);
-					testUtils.expectError();
-				} catch (error) {
-					expect(error).to.be.instanceof(ValidationError);
-					expect(error.context).to.be.an('array');
-					expect(error.context[0]).to.contain(`${property} is not within the allowed range`);
-					expect(model._changed).to.be.empty;
-				}
-			} else {
-				runTest(model);
-			}
-		}
-
-		it('tinyInt', function () {
-			const testType = 'tinyInt';
-			wrapRunTest(testType, -129, true);
-			wrapRunTest(testType, -128, false);
-			wrapRunTest(testType, 127, false);
-			wrapRunTest(testType, 128, true);
-
-			try {
-				testSchema[testType].unsigned = true;
-				wrapRunTest(testType, -1, true);
-				wrapRunTest(testType, 0, false);
-				wrapRunTest(testType, 255, false);
-				wrapRunTest(testType, 256, true);
-			} finally {
-				delete testSchema[testType].unsigned;
-			}
-
-			try {
-				testSchema[testType].validations = {
-					between: [0, 1]
-				};
-
-				wrapRunTest(testType, -1, true);
-				wrapRunTest(testType, 0, false);
-				wrapRunTest(testType, 1, false);
-				wrapRunTest(testType, 2, true);
-			} finally {
-				delete testSchema[testType].validations;
-			}
-		});
-
-		it('integer', function () {
-			const testType = 'integer';
-			wrapRunTest(testType, -2147483649, true);
-			wrapRunTest(testType, -2147483648, false);
-			wrapRunTest(testType, 2147483647, false);
-			wrapRunTest(testType, 2147483648, true);
-
-			try {
-				testSchema[testType].unsigned = true;
-				wrapRunTest(testType, -1, true);
-				wrapRunTest(testType, 0, false);
-				wrapRunTest(testType, 4294967295, false);
-				wrapRunTest(testType, 4294967296, true);
-			} finally {
-				delete testSchema[testType].unsigned;
-			}
-		});
-
-		it('float', function () {
-			const testType = 'float';
-			wrapRunTest(testType, -1000000.01, true);
-			wrapRunTest(testType, -999999, false);
-			wrapRunTest(testType, 999999, false);
-			wrapRunTest(testType, 1000000.01, true);
-
-			try {
-				testSchema[testType].unsigned = true;
-				wrapRunTest(testType, -1, true);
-				wrapRunTest(testType, 0, false);
-				wrapRunTest(testType, 999999, false);
-				wrapRunTest(testType, 1000000.01, true);
-			} finally {
-				delete testSchema[testType].unsigned;
-			}
-		});
-	});
-
-	it('numeric check handles empty values', function () {
-		const model = new FakeModel();
-
-		model.data.tinyInt = undefined;
+		const get = key => {
+			return original[key];
+		};
 
 		try {
-			runTest(model);
+			const model = {set, get};
+			validator.validateRow('__test__', model, {method: 'insert'});
 			testUtils.expectError();
 		} catch (error) {
 			expect(error).to.be.instanceof(ValidationError);
-			expect(error.context).to.be.an('array');
-			expect(error.context[0]).to.contain('tinyInt cannot be empty');
-			expect(model._changed).to.be.empty;
-		}
-
-		try {
-			testSchema.tinyInt.nullable = true;
-			runTest(model);
-			expect(model._changed.tinyInt).to.equal(null);
-
-			testSchema.tinyInt.fallback = 10;
-			runTest(model);
-			expect(model._changed.tinyInt).to.equal(10);
+			expect(error.context).to.deep.equal([
+				'__test__.text exceeds the maxLength of 24',
+				'__test__.created cannot be empty',
+				'__test__.grade is not within the allowed range'
+			]);
+			expect(diff).to.deep.equal({
+				optional: '51',
+				dropped: null
+			});
 		} finally {
-			delete testSchema.tinyInt.fallback;
-			delete testSchema.tinyInt.nullable;
-		}
-	});
-
-	it('boolean checks properly reject and coerce values', function () {
-		const model = new FakeModel();
-
-		model.data.boolean = undefined;
-
-		try {
-			runTest(model);
-			testUtils.expectError();
-		} catch (error) {
-			expect(error).to.be.instanceof(ValidationError);
-			expect(error.context).to.be.an('array');
-			expect(error.context[0]).to.contain('boolean cannot be empty');
-			expect(model._changed).to.be.empty;
-		}
-
-		model.data.boolean = false;
-		runTest(model);
-		expect(model._changed.boolean).to.equal(undefined);
-
-		// @ts-expect-error
-		model.data.boolean = 'false';
-		runTest(model);
-		expect(model._changed.boolean).to.equal(false);
-
-		// @ts-expect-error
-		model.data.boolean = '0';
-		runTest(model);
-		expect(model._changed.boolean).to.equal(false);
-
-		delete model._changed.boolean;
-		model.data.boolean = true;
-		runTest(model);
-		expect(model._changed.boolean).to.equal(undefined);
-
-		// @ts-expect-error
-		model.data.boolean = 'true';
-		runTest(model);
-		expect(model._changed.boolean).to.equal(true);
-
-		// @ts-expect-error
-		model.data.boolean = '1';
-		runTest(model);
-		expect(model._changed.boolean).to.equal(true);
-
-		try {
 			// @ts-expect-error
-			model.data.boolean = 'potato';
-			runTest(model);
-			testUtils.expectError();
-		} catch (error) {
-			expect(error).to.be.instanceof(ValidationError);
-			expect(error.context).to.be.an('array');
-			expect(error.context[0]).to.contain('boolean must be Boolean');
-		}
-	});
-
-	it('null-ish values are accepted for nullable columns', function () {
-		const model = new FakeModel();
-
-		model.data.optional = null;
-
-		runTest(model);
-	});
-
-	it('null-ish values are rejected for non-nullable columns', function () {
-		const model = new FakeModel();
-
-		model.data.id = null;
-
-		try {
-			runTest(model);
-			testUtils.expectError();
-		} catch (error) {
-			expect(error).to.be.instanceof(ValidationError);
-			expect(error.context).to.be.an('array');
-			expect(error.context[0]).to.contain('id cannot be null');
-			expect(model._changed).to.be.empty;
-		}
-
-		model.data.id = '';
-
-		try {
-			runTest(model);
-			testUtils.expectError();
-		} catch (error) {
-			expect(error).to.be.instanceof(ValidationError);
-			expect(error.context).to.be.an('array');
-			expect(error.context[0]).to.contain('id cannot be empty');
-			expect(model._changed).to.be.empty;
-		}
-	});
-
-	it('maxLength - strings and text blobs', function () {
-		const model = new FakeModel();
-
-		model.data.id = 'b'.repeat(25);
-
-		// Strings (part of the schema)
-		try {
-			runTest(model);
-			testUtils.expectError();
-		} catch (error) {
-			expect(error).to.be.instanceof(ValidationError);
-			expect(error.context).to.be.an('array');
-			expect(error.context[0]).to.contain('id exceeds the maxLength');
-			expect(model._changed).to.be.empty;
-		}
-
-		model.data.id = 'a'.repeat(24);
-		model.data.optional = 'b'.repeat(25);
-
-		// Blobs (part of the additional validations)
-		try {
-			runTest(model);
-			testUtils.expectError();
-		} catch (error) {
-			expect(error).to.be.instanceof(ValidationError);
-			expect(error.context).to.be.an('array');
-			expect(error.context[0]).to.contain('optional exceeds the maxLength');
-			expect(model._changed).to.be.empty;
-		}
-
-		// Generic
-		model.data.optional = 'o'.repeat(24);
-
-		runTest(model);
-		expect(model._changed).to.be.empty;
-	});
-
-	it('additional validation - between ', function () {
-		const model = new FakeModel();
-
-		model.data.dropped = 1;
-		runTest(model);
-
-		model.data.dropped = 40;
-		runTest(model);
-
-		model.data.dropped = 0;
-
-		try {
-			runTest(model);
-			testUtils.expectError();
-		} catch (error) {
-			expect(error).to.be.instanceof(ValidationError);
-			expect(error.context).to.be.an('array');
-			expect(error.context[0]).to.contain('dropped is not within the allowed range');
-			expect(model._changed).to.be.empty;
-		}
-
-		model.data.dropped = 41;
-
-		try {
-			runTest(model);
-			testUtils.expectError();
-		} catch (error) {
-			expect(error).to.be.instanceof(ValidationError);
-			expect(error.context).to.be.an('array');
-			expect(error.context[0]).to.contain('dropped is not within the allowed range');
-			expect(model._changed).to.be.empty;
+			delete schema.__test__;
 		}
 	});
 });
