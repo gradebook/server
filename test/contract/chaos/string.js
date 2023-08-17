ad6be6a89b3118ed880021f7fdb023590010694f
Vikas Potluri <vikaspotluri123.github@gmail.com>
2023-08-17 10:56:30 -0700

    update default string max and fix off-by-one error

--- test/contract/chaos/string.js
+++ test/contract/chaos/string.js
@@ -16,7 +16,7 @@ import {between, getTypes, Between} from './generic.js';
 /** @type {StringConstraints} */
 const DEFAULT_STRING_CONSTRAINTS = {
 	min: 0,
-	max: 500,
+	max: 50,
 	alphabet: 'abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789`~!@#$%^&*()_+{}[];\':",./<>?',
 };
 
@@ -25,13 +25,13 @@ const DEFAULT_STRING_CONSTRAINTS = {
 export function generateString(constraints = {}) {
 	const {min, max, alphabet: stringAlphabet} = Object.assign({}, DEFAULT_STRING_CONSTRAINTS, constraints);
 	const alphabet = stringAlphabet.split('');
-	const alphabetSize = alphabet.length;
+	const maxIndex = alphabet.length - 1;
 
 	let length = between(min, max);
 	let response = '';
 
 	while (length > 0) {
-		response += alphabet[between(0, alphabetSize)];
+		response += alphabet[between(0, maxIndex)];
 		length--;
 	}
 
