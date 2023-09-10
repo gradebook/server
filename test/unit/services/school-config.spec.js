acb9a42acbaf13656a3d0efc768f21e58d435138
Vikas Potluri <vikaspotluri123.github@gmail.com>
2023-09-09 18:44:12 -0700

    migrate school config to use native fetch
    done as a separate commit so it can be reverted when the upstream nock issue is fixed

--- test/unit/services/school-config.spec.js
+++ test/unit/services/school-config.spec.js
@@ -1,8 +1,10 @@
 // @ts-check
+import sinon from 'sinon';
 import makeNock from 'nock';
 import {expect} from 'chai';
 import {SchoolConfigService} from '../../../lib/services/school-config.js';
 import {importJson} from '../../../lib/utils/import-json.js';
+import {FetchError} from '../../../lib/utils/enhanced-fetch.js';
 
 const ENDPOINT_RESPONSE = await importJson('../../fixtures/school-configuration.json', import.meta.url);
 
@@ -13,6 +15,8 @@ describe('Unit > SchoolConfigurationService', function () {
 	let nock;
 	/** @type {SchoolConfigService} */
 	let service;
+	// @TODO: Remove this when nock supports native fetch
+	let stub;
 
 	before(function () {
 		nock = makeNock('http://nock.gbdev.cf');
@@ -20,6 +24,10 @@ describe('Unit > SchoolConfigurationService', function () {
 
 	beforeEach(function () {
 		service = new SchoolConfigService();
+		// @TODO: Remove these when nock supports native fetch
+		stub = sinon.stub();
+		// @ts-expect-error
+		service._fetch = stub;
 	});
 
 	after(function () {
@@ -28,6 +36,7 @@ describe('Unit > SchoolConfigurationService', function () {
 
 	it('init populates school and theme database', async function () {
 		nock.get(ENDPOINT_PATH).reply(200, ENDPOINT_RESPONSE);
+		stub.resolves(structuredClone(ENDPOINT_RESPONSE));
 		expect(service.getSchoolConfig('aggie').head).to.not.contain('aggie');
 
 		// .init() will always be called before any requests are allowed in
@@ -43,6 +52,7 @@ describe('Unit > SchoolConfigurationService', function () {
 
 	it('isPartnered', async function () {
 		nock.get(ENDPOINT_PATH).reply(200, ENDPOINT_RESPONSE);
+		stub.resolves(structuredClone(ENDPOINT_RESPONSE));
 		expect(await service.init()).to.be.true;
 
 		expect(service.isPartnered('aggie')).to.be.true;
@@ -51,7 +61,16 @@ describe('Unit > SchoolConfigurationService', function () {
 	});
 
 	it('refresh atomically updates school data', async function () {
+		let error = false;
 		nock.get(ENDPOINT_PATH).times(2).reply(200, ENDPOINT_RESPONSE);
+		stub.callsFake(() => {
+			if (error) {
+				throw new FetchError('Failed to fetch', 'ono');
+			}
+
+			return structuredClone(ENDPOINT_RESPONSE);
+		});
+
 		// @ts-ignore
 		const initialSchoolConfig = service._schoolConfigs;
 		expect(await service.init()).to.be.true;
@@ -65,6 +84,7 @@ describe('Unit > SchoolConfigurationService', function () {
 		expect(service._schoolConfigs, 'Object ref changed (request passed)').to.not.equal(initialSchoolConfig);
 
 		nock.get(ENDPOINT_PATH).reply(404, 'ono');
+		error = true;
 		// @ts-ignore
 		const firstRefreshConfig = service._schoolConfigs;
 
@@ -76,6 +96,7 @@ describe('Unit > SchoolConfigurationService', function () {
 
 	it('config cache is flushed when refreshing', async function () {
 		nock.get(ENDPOINT_PATH).reply(200, ENDPOINT_RESPONSE);
+		stub.resolves(structuredClone(ENDPOINT_RESPONSE));
 		// @ts-ignore
 		service._configCache.set('www', 'fake');
 
@@ -88,6 +109,7 @@ describe('Unit > SchoolConfigurationService', function () {
 
 	it('getSchoolConfig pulls from cache or generates markup', async function () {
 		nock.get(ENDPOINT_PATH).reply(200, ENDPOINT_RESPONSE);
+		stub.resolves(structuredClone(ENDPOINT_RESPONSE));
 		expect(await service.init(), 'Should have successfully loaded config').to.be.true;
 
 		// @ts-ignore
