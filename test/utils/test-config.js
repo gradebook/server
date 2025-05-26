050ed174fbac525ffcea952e6df1f636ec2223e1
Vikas Potluri <vikaspotluri123.github@gmail.com>
2025-05-25 17:59:01 -0700

    enable type checking for tests

--- test/utils/test-config.js
+++ test/utils/test-config.js
@@ -5,5 +5,5 @@ export const isCI = process.env.CI === 'true';
 export const isFunctionalTest = process.env.TEST_NAME === 'integration';
 export const FUNCTIONAL_TEST_DATABASE_NAME = 'host_real';
 export const FUNCTIONAL_TEST_DEFAULT_DATABASE = 'host_fake';
-export const TEST_DATABASE = isCI && isFunctionalTest ? FUNCTIONAL_TEST_DATABASE_NAME : undefined;
+export const TEST_DATABASE = isCI && isFunctionalTest ? FUNCTIONAL_TEST_DATABASE_NAME : '';
 export const TEST_HOST_NAME = 'host-a.gbdev.cf';
